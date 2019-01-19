a34cc9bc8f932a81db7edbfc9103013b9d215620
rusev9 <evg.rusev@gmail.com>
2019-01-19 22:03:33 +0200

    Deposit presentation layer - form

--- src/Common/BankManagementSystem.Common/BindingModels/Deposit/CreateDepositBindingModel.cs
+++ src/Common/BankManagementSystem.Common/BindingModels/Deposit/CreateDepositBindingModel.cs
@@ -1,11 +1,17 @@
 ﻿namespace BankManagementSystem.Common.BindingModels.Deposit
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateDepositBindingModel
     {
         public int Id { get; set; }
 
+        // TODO: Develop constants
+        [Required]
+        [Range(10.00, 10000.00)]
         public decimal Amount { get; set; }
 
+        [Required]
         public int CreditCardId { get; set; }
     }
 }
