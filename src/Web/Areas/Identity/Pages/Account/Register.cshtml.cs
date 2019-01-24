5fabeaa2046109cc046ba8fb376878f8857f3e46
rusev9 <evg.rusev@gmail.com>
2019-01-24 12:06:54 +0200

    Fix Administrator-register bug

--- src/Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -73,9 +73,9 @@ namespace BankManagementSystem.Web.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    
                     await _userManager.AddToRoleAsync(user, userRole);
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
 
                     return LocalRedirect(returnUrl);
                 }
